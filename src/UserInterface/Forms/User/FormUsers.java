e60f45a42c189c1500cb8a2de0b2a44f9bb09549
Fernando Calmet <fercalmet@gmail.com>
2020-10-04 22:03:49 -0500

    Update FormUsers.java

--- src/UserInterface/Forms/User/FormUsers.java
+++ src/UserInterface/Forms/User/FormUsers.java
@@ -1,4 +1,12 @@
 package UserInterface.Forms.User;
 
+import Domain.Models.UserModel;
+
 public class FormUsers {
+    private UserModel userModel = new UserModel();
+
+    private void loadUserData()
+    {
+
+    }
 }
